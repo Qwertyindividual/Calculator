579f4ea1454dbaddf499bef7d16d8bc29df45619
Qwerty individual <brandtkauthar@gmail.com>
2022-11-26 22:55:40 +0200

    update

--- calculator.js
+++ calculator.js
@@ -6,26 +6,40 @@ let calculate = (number) => {
 };
 let addToSum = (operator) => {
   sum.value += operator;
-  allowDecimal = true;
+ 
 };
 
 let evaluateSum = () => {
   try {
     sum.value = eval(sum.value);
   } catch (err) {
+    // sum.innerText = "Invalid"
     alert(
       " Please enter a valid equation."
     );
-    
-  } 
-};
+  }
+} 
 
 function clearSum() {
   sum.value = "0";
-  allowDecimal = true;
 }
 
 function deleteSum() {
   sum.value = sum.value.slice(0, -1);
+};
+
+function inputDecimal(dot) {
+  // If the `displayValue` property does not contain a decimal point
+  if (!calculator.displayValue.includes(dot)) {
+    // Append the decimal point
+    calculator.displayValue += dot;
+  }
 }
+// function decimal() {
+//   if('.' > 1) {
+//   return " ";
+//   } else {
+//     return "."
+//   }
+// };
 
